89d6d49841ae261f7d682efcdaf292283d82329c
agrwl-prankit <agrwnlprankit77@gmail.com>
2021-04-29 19:34:12 +0530

    add pop(), show() and size() for stackUsingDLL

--- src/stackUsingDLL/StackUsingDLL.java
+++ src/stackUsingDLL/StackUsingDLL.java
@@ -21,8 +21,50 @@ public class StackUsingDLL {
         System.out.println(data + " inserted successfully");
     }
 
+    public void pop(){
+        if (isEmpty()) System.out.println("Stack is already empty");
+        else {
+            DoublyNode temp = head, previous = null;
+            while (temp.getNext() != null){
+                previous = temp;
+                temp = temp.getNext();
+            }
+            if (previous == null) head = null;
+            else previous.setNext(null);
+            tail = previous;
+            System.out.println(temp.getData() + " removed successfully");
+        }
+    }
+
     public void peek(){
-        System.out.println("peek element is: " + tail.getData());
+        if (isEmpty()) System.out.println("Stack is empty");
+        else System.out.println("peek element is: " + tail.getData());
+    }
+
+    public void show(){
+        if (isEmpty()) System.out.println("Stack is empty");
+        else {
+            DoublyNode temp = head;
+            while (temp != null){
+                System.out.print(temp.getData());
+                temp = temp.getNext();
+                if (temp != null) System.out.print("->");
+            }
+            System.out.println();
+        }
+    }
+
+    public void size(){
+        if (isEmpty()) System.out.println("No element available");
+        else{
+            DoublyNode temp = head;
+            int size = 0;
+            while (temp != null){
+                size++;
+                temp = temp.getNext();
+            }
+            System.out.println("Size of stack is: " + size);
+        }
     }
 
     public boolean isEmpty(){
--- src/stackUsingDLL/stackUsingDLLMain.java
+++ src/stackUsingDLL/stackUsingDLLMain.java
@@ -4,10 +4,18 @@ public class stackUsingDLLMain {
     public static void main(String[] args) {
         StackUsingDLL stack = new StackUsingDLL();
         stack.push(1);
-        stack.peek();
         stack.push(2);
         stack.push(3);
         stack.push(4);
+        stack.show();
+        stack.pop();
+        stack.size();
+        stack.pop();
+        stack.pop();
+        stack.size();
+        stack.pop();
         stack.peek();
+        stack.show();
+        stack.size();
     }
 }
